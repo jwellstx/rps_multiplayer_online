636503a141f3ea4bb5fcd1fcfc991ee9838cc70d
JWeez <jwellstx@gmail.com>
2020-03-13 02:24:07 -0500

    added rps logic, player selection, and scoring

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,14 @@
+// TODO: make sure third person doesnt reset the stats
+// comment, reorganize, sub calls, general cleanup
+
 var rps = {
     name: null,
     pwd: "",
     currentGameIndex: 0,
     player: 0,
+    playerNumber: "",
     selectedMove: false,
+    gameInProgress: false,
 
     // Your web app's Firebase configuration
     firebaseConfig: {
@@ -19,10 +24,43 @@ var rps = {
     Start: function () {
         firebase.initializeApp(this.firebaseConfig);
         var db = firebase.database();
+        db.ref().update({
+            reset: {
+                state: false,
+                dcplayer: ""
+            }
+        });
+
+        db.ref("reset").on("value", snapshot => {
+            var reset = snapshot.val();
+            if (reset.state) {
+                db.ref().set(null);
+                if (rps.playerNumber !== reset.dcplayer) {
+                    location.reload();
+                }
+                db.ref("reset").update({
+                    state: false,
+                    dcplayer: ""
+                });
+
+                alert("The other player has left the game!");
+            }
+        });
+
+        db.ref("users").once("value", snapshot => {
+            var dbRef = snapshot.val();
+            console.log(snapshot, dbRef);
+            if (snapshot.child("player1").exists() && snapshot.child("player2").exists() && dbRef !== null) {
+                $("#p1buttons, #p2buttons, .spec").hide();
+                rps.gameInProgress = true;
+            }
+        });
 
         $("#player1Submit").on("click", function () {
             rps.player = $("#player1Name").val().trim();
-            $(".p2login").empty();
+            rps.playerNumber = "player1";
+            $(".p2login, .p2sel").remove();
+
             db.ref("users/player1").set({
                 name: rps.player,
                 present: true,
@@ -31,10 +69,12 @@ var rps = {
 
         $("#player2Submit").on("click", function () {
             rps.player = $("#player2Name").val().trim();
-            $(".p1login").empty();
+            rps.playerNumber = "player2";
+            $(".p1login, .p1sel").remove();
+
             db.ref("users/player2").set({
                 name: rps.player,
-                preset: true
+                present: true
             });
         });
 
@@ -43,15 +83,34 @@ var rps = {
             if (snapshot.child("player1").exists()) {
                 $(".p1login").empty();
                 $(".p1selected").html("Player 1 selected, waiting on player2!");
+                $("#p1Name").html(dbRef.player1.name);
+
+                window.onunload = function () {
+                    db.ref("reset").update({
+                        state: true,
+                        dcplayer: rps.playerNumber,
+                    });
+                }
             }
             if (snapshot.child("player2").exists()) {
                 $(".p2login").empty();
                 $(".p2selected").html("Player 2 selected, waiting on player1!");
+                $("#p2Name").html(dbRef.player2.name);
+
+                window.onunload = function () {
+                    db.ref("reset").update({
+                        state: true,
+                        dcplayer: rps.playerNumber,
+                    });
+                }
             }
             if (snapshot.child("player1").exists() && snapshot.child("player2").exists()) {
                 $(".p1selected, .p2selected").remove();
                 console.log("we are ready to play!!");
-                rps.StartGame();
+                console.log(rps.gameInProgress);
+                if (!rps.gameInProgress) {
+                    rps.StartGame(db);
+                }
             }
         });
 
@@ -73,6 +132,16 @@ var rps = {
             $('#submitCmt').click();
             event.preventDefault();
         });
+        $(document).on("keyup", "#player1Name", function (event) {
+            if (event.key !== "Enter") return;
+            $('#player1Submit').click();
+            event.preventDefault();
+        });
+        $(document).on("keyup", "#player2Name", function (event) {
+            if (event.key !== "Enter") return;
+            $('#player2Submit').click();
+            event.preventDefault();
+        });
 
         db.ref("messages").on("child_added", snapshot => {
             var cont = `
@@ -87,13 +156,119 @@ var rps = {
 
         });
     },
-    StartGame: function () {
+    StartGame: function (db) {
+        var picked = false;
+        db.ref().update({
+            game: {
+                ties: 0,
+                player1wins: 0,
+                player2wins: 0,
+                player1loss: 0,
+                player2loss: 0
+            }
+        });
 
+        // if left, tell the game we need to reset
+        // window.onunload = function () {
+        //     if (rps.playerNumber) {
+        //         db.ref("reset").update({
+        //             state: true,
+        //             dcplayer: rps.playerNumber,
+        //         });
+        //     }
+        // }
+
+        // db.ref("reset").on("value", snapshot => {
+        //     var reset = snapshot.val();
+        //     if (reset.state) {
+        //         db.ref().set(null);
+        //         if (rps.playerNumber !== reset.dcplayer) {
+        //             location.reload();
+        //         }
+        //         db.ref("reset").update({
+        //             state: false,
+        //             dcplayer: ""
+        //         });
+
+        //         alert("The other player has left the game!");
+        //     }
+        // });
+
+        db.ref("game").on("value", snapshot => {
+            var dbRef = snapshot.val();
+
+            $("#p1wins").text(dbRef.player1wins);
+            $("#p1losses").text(dbRef.player1loss);
+            $("#p2wins").text(dbRef.player2wins);
+            $("#p2losses").text(dbRef.player2loss);
+        });
+
+        $(".p1sel").on("click", function () {
+            console.log(picked, $(this).val());
+            if (!picked) {
+                console.log(rps.playerNumber);
+                db.ref("choices/" + rps.playerNumber).update({
+                    choice: $(this).val()
+                });
+                picked = true;
+            }
+        });
+        $(".p2sel").on("click", function () {
+            console.log(picked, $(this).val());
+            if (!picked) {
+                console.log(rps.playerNumber);
+                db.ref("choices/" + rps.playerNumber).update({
+                    choice: $(this).val()
+                });
+                picked = true;
+            }
+        });
+
+        db.ref("choices").on("value", snapshot => {
+            var dbRef = snapshot.val();
+            console.log(dbRef);
+            if (dbRef !== null && snapshot.child("player1/choice").exists() && snapshot.child("player2/choice").exists()) {
+                var results = dbRef.player1.choice + dbRef.player2.choice;
+                db.ref("game").once("value", snap => {
+                    var dbRefGame = snap.val();
+                    switch (results) {
+                        case "ss":
+                        case "pp":
+                        case "rr":
+                            db.ref("game").update({
+                                ties: parseInt(dbRefGame.ties) + 1
+                            });
+                            break;
+                        case "sp":
+                        case "pr":
+                        case "rs":
+                            db.ref("game").update({
+                                player1wins: parseInt(dbRefGame.player1wins) + 1,
+                                player2loss: parseInt(dbRefGame.player2loss) + 1
+                            });
+                            break;
+                        case "sr":
+                        case "rp":
+                        case "ps":
+                            db.ref("game").update({
+                                player1loss: parseInt(dbRefGame.player1loss) + 1,
+                                player2wins: parseInt(dbRefGame.player2wins) + 1
+                            });
+                            break;
+                        default:
+                            alert("Not a valid entry");  // shouldnt ever hit this
+                    }
+
+                }).then(function () { picked = false; });
+                db.ref("choices/player1/choice").set(null);
+                db.ref("choices/player2/choice").set(null);
+            }
+        });
     }
 }
 
 $(function () {
-
+    // to empty firebase db.ref("/").set(null);
     rps.Start();
 });
 
